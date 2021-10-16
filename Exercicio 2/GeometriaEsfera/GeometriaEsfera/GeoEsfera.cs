5d941d9a5d5237171e1f1a378ded561c0449817e
Bruno <bruno.soarez08@gmail.com>
2021-10-16 12:52:34 -0300

    modificação Exercicio 2

---
+++ Exercicio 2/GeometriaEsfera/GeometriaEsfera/GeoEsfera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometriaEsfera
+{
+    class GeoEsfera
+    {
+        private double PI;
+        private double Raio;
+
+        public GeoEsfera()
+        {
+
+        }
+        public GeoEsfera(double Raio)
+        {
+            SetPi(3.14);
+            SetRaio(Raio);
+        }
+        public double Vol_Esfera()
+        {
+            return (4 / 3) * GetPi() * (GetRaio()* GetRaio()* GetRaio());
+        }
+        public void SetRaio(double raio) => this.Raio = raio;
+        public double GetRaio() => this.Raio;
+        public double GetPi() => this.PI;
+        private void SetPi(double PI) => this.PI = PI;
+    }
+}
